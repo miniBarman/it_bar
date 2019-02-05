3939fa26894844cf53b81c256544352ac305e5c7
mini_barman <stovgsb06@mail.ru>
2019-02-06 00:23:36 +0500

    user creation

---
+++ src/main/java/com/project/itbar/controller/RegistrationController.java
@@ -0,0 +1,37 @@
+package com.project.itbar.controller;
+
+import com.project.itbar.domain.User;
+import com.project.itbar.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("sysMessage", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
